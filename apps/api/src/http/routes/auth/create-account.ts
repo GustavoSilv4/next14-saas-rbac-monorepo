0c15bf4dbdc97f49f49665c83c063906312e9cd5
Gustavo Silva <gustavosilv4.contato@gmail.com>
2024-04-27 15:57:06 -0300

    [api] rota para cadastro de usuario

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -1,6 +1,9 @@
+import { hash } from 'bcryptjs'
 import { FastifyInstance } from 'fastify'
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
 import z from 'zod'
+
+import { prisma } from '@/lib/prisma'
 
 export async function createAccount(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
@@ -14,8 +17,30 @@ export async function createAccount(app: FastifyInstance) {
         }),
       },
     },
-    () => {
-      return 'User Created'
+    async (req, reply) => {
+      const { email, name, password } = req.body
+
+      const userWithSameEmail = await prisma.user.findUnique({
+        where: { email },
+      })
+
+      if (userWithSameEmail) {
+        return reply
+          .status(400)
+          .send({ message: 'user with same e-mail already exists.' })
+      }
+
+      const hashedPassword = await hash(password, 6)
+
+      await prisma.user.create({
+        data: {
+          email,
+          name,
+          passwordHash: hashedPassword,
+        },
+      })
+
+      return reply.status(201).send()
     },
   )
 }
