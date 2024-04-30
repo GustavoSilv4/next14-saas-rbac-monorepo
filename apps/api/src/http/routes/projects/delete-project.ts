d605e92a37f8411afc20999d33ccb704bd02b099
Gustavo Silva <gustavosilv4.contato@gmail.com>
2024-04-29 23:19:44 -0300

    [api] rota: deletando um projeto

---
+++ apps/api/src/http/routes/projects/delete-project.ts
@@ -0,0 +1,69 @@
+import { auth } from "@/http/middlewares/auth"
+import { prisma } from "@/lib/prisma"
+import { FastifyInstance } from "fastify"
+import { ZodTypeProvider } from "fastify-type-provider-zod"
+import z from "zod"
+import { BadRequestError } from "../_errors/bad-request-error"
+import { getUserPermissions } from "@/utils/get-user-permissions"
+import { UnauthorizedError } from "../_errors/unauthorized-error"
+import { projectSchema } from "@saas/auth/src/models/project"
+
+export async function deleteProject(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .delete(
+      "/organizations/:slug/projects/:projectId",
+      {
+        schema: {
+          tags: ["projects"],
+          summary: "Delete a project",
+          security: [
+            {
+              bearerAuth: [],
+            },
+          ],
+          params: z.object({
+            slug: z.string(),
+            projectId: z.string().uuid(),
+          }),
+          response: {
+            204: z.null(),
+          },
+        },
+      },
+      async (req, reply) => {
+        const { slug, projectId } = req.params
+        const userId = await req.getCurrentUserId()
+        const { membership, organization } = await req.getUserMembership(slug)
+
+        const project = await prisma.project.findUnique({
+          where: {
+            id: projectId,
+            organizationId: organization.id,
+          },
+        })
+
+        if (!project) {
+          throw new BadRequestError("Project not found")
+        }
+
+        const { cannot } = getUserPermissions(userId, membership.role)
+        const authProject = projectSchema.parse(project)
+
+        if (cannot("delete", authProject)) {
+          throw new UnauthorizedError(
+            "You are not allowed to delete this projects."
+          )
+        }
+
+        await prisma.project.delete({
+          where: {
+            id: projectId,
+          },
+        })
+
+        return reply.status(204).send()
+      }
+    )
+}
